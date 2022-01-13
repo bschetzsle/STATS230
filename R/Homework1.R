bb599837b1c05c655eebaf3b6f89d38dc52de8f3
bschetzsle <bschetzs@uci.edu>
2022-01-13 13:20:28 -0800

    Adding Homework1.R

--- R/Homework1.R
+++ R/Homework1.R
@@ -1,3 +1,14 @@
+#'Multiply three matrices
+#'
+#' @param A a M x N matrix
+#' @param B a N x P matrix
+#' @param x a P x 1 vector/matrix
+#' @param method specify the order of multiplication
+#' @return If method=1, returns (AB)x, otherwise returns A(Bx)
+#' @examples
+#' matrix_prod(A, B, x, 1))
+#' matrix_prod(A, B, x, 2))
+
 matrix_prod = function(A, B, x, method=1){
   if(dim(A)[2] != dim(B)[1] || dim(B)[2] != dim(x)[1]){
     print("Non-conforming matrices!!")
